9344c2625c63a3b42252346dac1e939aebad3119
skyriv213 <kimminsoo213@dankook.ac.kr>
2024-04-28 00:21:01 +0900

    feature <(page)> :  페이지 만들기
    페이지 이미지 저장
    
    #14

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { createGlobalStyle } from "styled-components";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "./App.css";
+
+function GlobalStyle() {
+  return (
+    <style>
+      {`
+        body {
+          background: url('https://images.pexels.com/photos/924824/pexels-photo-924824.jpeg?cs=srgb&dl=pexels-jakub-novacek-924824.jpg&fm=jpg') no-repeat center center;
+          justify-content: center;
+        }
+      `}
+    </style>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hihih
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <Router>
+        <Header />
+        <TransitionGroup className="transition-group">
+          <CSSTransition classNames="fade" timeout={500}>
+            <Routes>
+              <Route path="/Mid-Term-Project/" element={<Home />} />
+              <Route path="/Mid-Term-Project/about" element={<About />} />
+              <Route path="/Mid-Term-Project/contact" element={<Contact />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </Router>
     </div>
   );
 }
